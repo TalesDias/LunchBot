eb9682ad1b6d54c1cca0422d71f74e59c7a6acca
TalesDias <dtales15@gmail.com>
2026-02-28 16:21:31 -0300

    Sending a test message to whatsapp

---
+++ whatsapp.py
@@ -0,0 +1,31 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+INSTANCE_ID = os.getenv("GREEN_API_INSTANCE_ID")
+API_TOKEN   = os.getenv("GREEN_API_TOKEN")
+GROUP_ID    = os.getenv("GREEN_API_GROUP_ID")
+
+BASE_URL = f"https://api.green-api.com/waInstance{INSTANCE_ID}"
+
+
+def send_message(text: str) -> bool:
+    url = f"{BASE_URL}/sendMessage/{API_TOKEN}"
+    payload = {
+        "chatId": GROUP_ID,
+        "message": text
+    }
+    response = requests.post(url, json=payload)
+    
+    return response.status_code == 200
+
+
+if __name__ == "__main__":
+    success = send_message("Testing Green API")
+    
+    if success:
+        print("Success")
+    else: 
+        print("Something went wrong")
